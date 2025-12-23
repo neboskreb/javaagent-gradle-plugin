cf3679ca908661ac592f83227779a78eb44ab7fe
John Y. Pazekha <neboskreb@narod.ru>
2025-12-23 13:25:48 +0100

    Release version 1.2.6

--- example-projects/settings.gradle.kts
+++ example-projects/settings.gradle.kts
@@ -13,7 +13,7 @@ pluginManagement {
         id("com.android.application").version("8.13.2")
         id("com.android.library").version("8.13.2")
         id("de.mannodermaus.android-junit5").version("1.14.0.0")
-        id("io.github.neboskreb.javaagent").version("1.2.6-SNAPSHOT")
+        id("io.github.neboskreb.javaagent").version("1.2.6")
     }
 }
 
